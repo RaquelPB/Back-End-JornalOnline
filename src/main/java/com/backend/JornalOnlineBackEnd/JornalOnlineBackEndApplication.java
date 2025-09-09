2c4d1faee2c1c31251576a144b80b2da26b2ba28
RAQUEL PEREIRA BEZERRA <raquel.bezerra@alunos.sc.senac.br>
2025-09-08 21:14:50 -0300

    Primeiro Commit'

---
+++ src/main/java/com/backend/JornalOnlineBackEnd/JornalOnlineBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.backend.JornalOnlineBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JornalOnlineBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JornalOnlineBackEndApplication.class, args);
+	}
+
+}
